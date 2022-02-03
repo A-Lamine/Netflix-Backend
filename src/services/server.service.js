e4ca96125551c5adf67067b345c9fa45afa58f18
Lamine ADEL <lamine.adel@ynov.com>
2022-02-03 02:00:41 +0100

    init

---
+++ src/services/server.service.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const config = require("../configs");
+const port = config.server.port;
+const apiRouter = require("../routes");
+
+
+const app = express()
+app.use(bodyParser.json());
+
+app.use("/api/v1/", apiRouter)
+
+exports.start = () => {
+  app.listen(port, (err) => {
+    if (err) {
+      console.log(`Errors: ${err}`)
+      process.exit(-1);
+    }
+    console.log(`app is runnning on port ${port}`);
+  })
+}
